09f8af01035ab0d74cf257100f871e04f8b74283
Terver Aosu <37598493+truetechcode@users.noreply.github.com>
2020-12-16 12:33:52 +0100

    create controllers for post and get requests

---
+++ controllers/short_url.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const urlService = require('../services/short_url');
+
+let router = express.Router();
+
+router.get('/:shortcode', urlService.getUserById);
+
+router.get('/:shortcode/stats', urlService.getUserById);
+
+router.post('/', urlService.createShortUrl);
+
+module.exports = router;
